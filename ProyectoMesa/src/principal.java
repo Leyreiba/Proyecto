c25499f3a06dcceb82f178a4cb28ab7acdada53c
leire.ibanez <leire.ibanez@opendeusto.es>
2016-12-03 12:53:34 +0100

    scrollPane-scrollbar list1 list2

--- ProyectoMesa/src/principal.java
+++ ProyectoMesa/src/principal.java
@@ -10,6 +10,7 @@ import BasesDeDatos.BD;
 
 import javax.swing.JList;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
@@ -20,12 +21,16 @@ import javax.swing.JFileChooser;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.SystemColor;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
 
 public class principal extends JFrame {
 
@@ -70,36 +75,140 @@ public class principal extends JFrame {
 		 * pasar el nombre al textfield de abajo
 		 */
 		
+		/**
+		 * Creo un thread para que mientras se ejecute el programa se 
+		 * siga ejecutando el dato del jTextField.
+		 * Selecciono el texto de la lista y se escribe en el jTextField.
+		 * Cada vez que selecciono una nueva canción, los threads anteriores se paran y 
+		 * se crea el de la nueva selección
+		 * */
+		
+		
+		/**lista 1
+		 * */
+		ArrayList<Thread> aThreads = new ArrayList<Thread>();
 		DefaultListModel<String> dlm = new DefaultListModel<String>();
 		JList<String> list1 = new JList<String>(dlm);
+		list1.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent arg0) {
+				for(int i=0;i<aThreads.size();i++){
+					aThreads.get(i).stop();
+					aThreads.remove(i);
+				}
+				textField1.setText(list1.getSelectedValue());
+				aThreads.add(new Thread ( new Runnable() {
+					
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+					    char texto[] = textField1.getText().toCharArray();
+					    while(true){
+					    	/**
+					    	 * Transformo el array a uno de string y desplazo el texto seleccionado hacia 
+					    	 * la izquierda
+					    	 * */
+					    	textField1.setText(String.valueOf(texto));
+					    	char primera = texto[0];
+					    	for(int i=0;i<texto.length-1;i++)
+					    		texto[i] = texto[i+1];
+					    	texto[texto.length-1]=primera;
+					    	try {
+								Thread.sleep(300);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+					    }
+					}
+				}));
+				aThreads.get(0).start();
+			}
+		});
 		list1.setToolTipText("");
-		list1.setBounds(37, 68, 140, 203);
+//		list1.setBounds(37, 68, 140, 203);
 		contentPane.add(list1);
 		
+	
 		
-		//DefaultListModel<principal> dlmlist1 = (DefaultListModel<principal>)list1.getModel();
-//		JScrollPane barraDesplazamiento = new JScrollPane(list); 
-//		barraDesplazamiento.setBounds(10,30,200,110); 
-//		list.add(barraDesplazamiento); 
+		/**
+		 * Añadimos una JScrollPane a la lista
+		 * */
 		
-		JList<String> list2 = new JList<String>(dlm);
-		list2.setBounds(324, 68, 140, 203);
-		contentPane.add(list2);
+		 list1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+         JScrollPane scrollPane = new JScrollPane(list1);
+         scrollPane.setBounds(37, 68, 140, 203);
+         contentPane.add(scrollPane);
+         
+         
+         
+         /*** lista2
+          * */
+ 		
+ 		ArrayList<Thread> aThreads2 = new ArrayList<Thread>();
+ 		DefaultListModel<String> dlm2 = new DefaultListModel<String>();
+ 		JList<String> list2 = new JList<String>(dlm2);
+ 		list2.addListSelectionListener(new ListSelectionListener() {
+ 			public void valueChanged(ListSelectionEvent arg1) {
+ 				for(int a=0;a<aThreads2.size();a++){
+ 					aThreads2.get(a).stop();
+ 					aThreads2.remove(a);
+ 				}
+ 				textField2.setText(list2.getSelectedValue());
+ 				aThreads2.add(new Thread ( new Runnable() {
+ 					
+ 					@Override
+ 					public void run() {
+ 						// TODO Auto-generated method stub
+ 					    char texto2[] = textField2.getText().toCharArray();
+ 					    while(true){
+ 					    	/**
+ 					    	 * Transformo el array a uno de string y desplazo el texto seleccionado hacia 
+ 					    	 * la izquierda
+ 					    	 * */
+ 					    	textField2.setText(String.valueOf(texto2));
+ 					    	char primera = texto2[0];
+ 					    	for(int a=0;a<texto2.length-1;a++)
+ 					    		texto2[a] = texto2[a+1];
+ 					    	texto2[texto2.length-1]=primera;
+ 					    	try {
+ 								Thread.sleep(300);
+ 							} catch (InterruptedException e) {
+ 								// TODO Auto-generated catch block
+ 								e.printStackTrace();
+ 							}
+ 					    }
+ 					}
+ 				}));
+ 				aThreads2.get(0).start();
+ 			}
+ 		});
+ 		list2.setToolTipText("");
+ 		list2.setBounds(37, 68, 140, 203);
+ 		contentPane.add(list2);
 		
-			
-
+				
+ 		
 		
+		list2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane scrollPane2 = new JScrollPane(list2);
+        scrollPane2.setBounds(324, 68, 140, 203);
+        contentPane.add(scrollPane2);
+		
+	
+				
 		
 		textField1 = new JTextField();
 		textField1.setBounds(37, 286, 140, 26);
 		contentPane.add(textField1);
 		textField1.setColumns(10);
+		 
 		
 		textField2 = new JTextField();
 		textField2.setColumns(10);
 		textField2.setBounds(324, 286, 140, 26);
 		contentPane.add(textField2);
 		
+				
 		JSlider slider = new JSlider();
 		slider.setBounds(187, 286, 128, 26);
 		contentPane.add(slider);
@@ -185,7 +294,11 @@ public class principal extends JFrame {
 						dlm.addElement(cancionesSeleccionadas[i].getName());
 						System.out.println(cancionesSeleccionadas[i].getName());
 					}
-					list1.setModel(dlm);
+					dlm = (DefaultListModel<String>) list2.getModel(); 
+					for(int i=0;i<cancionesSeleccionadas.length;i++){
+						dlm.addElement(cancionesSeleccionadas[i].getName());
+						System.out.println(cancionesSeleccionadas[i].getName());
+					}
 				}
 				
 			}
